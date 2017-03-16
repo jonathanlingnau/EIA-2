d0b814ab2532bd1161f2304e8f02d9c970d1fd2c
jonat <jonat@Jonny-Laptop.hfu-open.fu.hs-furtwangen.de>
2017-03-16 14:39:56 +0100

    Aufgabe 0.5

---
+++ Aufgabe 0/aufgabe0.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var gruß = "Herzlich Willkommen";
+    var getName = prompt("Bitte geben sie Ihren Namen ein");
+    document.getElementById("Gruß").innerHTML = gruß + " " + getName + "!";
+});
+//# sourceMappingURL=aufgabe0.js.map
---
+++ Aufgabe 0/aufgabe0.ts
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var gruß = "Herzlich Willkommen" ;
+    var getName = prompt ("Bitte geben sie Ihren Namen ein");
+    document.getElementById("Gruß").innerHTML = gruß + " " + getName + "!";
+    
+    });
