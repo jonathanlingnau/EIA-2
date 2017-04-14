79015a32e101767df27f2a8c7ece90e8e6b18ce4
jonat <jonat@Jonny-Laptop.localdomain>
2017-04-14 21:26:47 +0200

    sicherung

--- Aufgabe 4/aufgabe4.ts
+++ Aufgabe 4/aufgabe4.ts
@@ -27,6 +27,7 @@ namespace Canvas {
     }
 
     function drawMountain(_x: number, _y: number, _fillColor: string): void {
+       
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.moveTo(_x - 200, _y );
@@ -38,9 +39,22 @@ namespace Canvas {
     }
     
     function drawBlume(_x: number, _y: number, _fillColor: string): void {
-        crc2.beginPath();
-        crc2.fillStyle = _fillColor;
-        crc2.fillRect(0, 420, 1270, 420);
+        
+            crc2.beginPath();
+            crc2.moveTo(20, 50);
+            
+            crc2.lineTo(20, 50);
+            crc2.arc(90, 50, 30, Math.PI * 8 / 6, Math.PI *  6/ 10);
+            crc2.closePath();
+            crc2.fillStyle = "#ff0000";
+//          crc2.lineWidth = 1;
+            crc2.lineJoin = 'round';
+            
+            crc2.fill();
+        
+//        crc2.beginPath();
+//        crc2.fillStyle = _fillColor;
+//        crc2.fillRect(0, 420, 1270, 420);
 //        crc2.moveTo(_x - 10, _y );
 //        crc2.lineTo(_x + 10, _y - 50);
 //        crc2.lineTo(_x + 20, _y - 50);
